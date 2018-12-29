97b7fbb8b93cbeecd1853751b37cc737a66ce59c
suporka <zxpscau@163.com>
2018-12-29 14:36:05 +0800

    :art: 改进代码的结构/格式  改进git-commit文件

--- commit/promps.js
+++ commit/promps.js
@@ -7,67 +7,67 @@ module.exports = {
       choices: [
         {
           name: "✨  引入新特性",
-          value: ":sparkles: Introducing new features"
+          value: ":sparkles: 引入新特性 "
         },
         {
           name: "🎨  改进代码的结构/格式",
-          value: ":art: Optimization"
+          value: ":art: 改进代码的结构/格式 "
         },
         {
           name: "🐛  修复 bug",
-          value: ":bug: Fix bug"
+          value: ":bug: 修复 bug "
         },
         {
           name: "⚡️  提升性能",
-          value: ":zap: Improve performance"
+          value: ":zap: 提升性能 "
         },
         {
           name: "🔥  删除代码或文件",
-          value: ":new: Delete file"
+          value: ":new: 删除代码或文件 "
         },
         {
           name: "💄  更新用户界面和样式文件",
-          value: ":lipstick: Update web page"
+          value: ":lipstick: 更新用户界面和样式文件"
         },
         {
           name: "🔧  更改配置文件",
-          value: ":wrench: Update config file"
+          value: ":wrench: 更改配置文件 "
         },
         {
           name: "🚧  工作进行中",
-          value: ":construction: Work in progress"
+          value: ":construction: 工作进行中 "
         },
         {
           name: "📝  撰写文档",
-          value: ":memo: Writing a document"
+          value: ":memo: 撰写文档 "
         },
         {
           name: "✅  增加测试",
-          value: ":white_check_mark: Increase test"
+          value: ":white_check_mark: 增加测试 "
         },
         {
           name: "📦  更新打包文件",
-          value: ":package: Update package file"
+          value: ":package: 更新打包文件 "
         },
         {
           name: "🎉  初次提交",
-          value: ":tada: First commit"
+          value: ":tada: 初次提交 "
         },
         {
           name: "🔖  发布/版本标签",
-          value: ":bookmark: Publish"
+          value: ":bookmark: 发布/版本标签"
         },
         {
           name: "🚀  部署功能",
-          value: ":rocket:"
+          value: ":rocket: 部署功能 "
         },
         {
           name: "👷  CI编译系统",
-          value: ":construction_worker:"
+          value: ":construction_worker: CI编译系统 "
         },
         {
           name: "🌐  国际化与本地化",
-          value: ":globe_with_meridians:"
+          value: ":globe_with_meridians: 国际化与本地化 "
         }
       ]
     }
