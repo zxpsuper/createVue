e645b26d97171ef2787a97a53efbe24e08355134
zxpsuper <zxpscau@163.com>
2020-03-19 15:55:03 +0800

    :art: 改进代码的结构/格式  修改提交样式

--- commit/git-commit.js
+++ commit/git-commit.js
@@ -1,14 +1,14 @@
-const shell = require("shelljs");
-const inquirer = require("inquirer");
-const prompsConfig = require("./promps");
+const shell = require('shelljs')
+const inquirer = require('inquirer')
+const prompsConfig = require('./promps')
 
 async function gitCommit() {
-  let { type } = await inquirer.prompt(prompsConfig.ciType);
-  let { msg } = await inquirer.prompt(prompsConfig.ciMsg);
+    let { type } = await inquirer.prompt(prompsConfig.ciType)
+    let { msg } = await inquirer.prompt(prompsConfig.ciMsg)
 
-  shell.exec(`git commit -m "${type} ${msg}"`, function() {
-    console.log(`\n提交脚本: git commit -m "${type}:${msg}"`);
-  });
+    shell.exec(`git commit -m "${type} ${msg}"`, function() {
+        console.log(`\n提交脚本: git commit -m "${type}: ${msg}"`)
+    })
 }
 
-gitCommit();
+gitCommit()
